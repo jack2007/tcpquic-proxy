68fe3e2cfd14cf2de6ef80cf21cc5e8e3e4c7fef
jack <jack2007@users.noreply.github.com>
2026-07-03 13:18:05 +0800

    Fix Windows SDK forced include architecture

--- cmake/windows7_msquic_compat.h
+++ cmake/windows7_msquic_compat.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #if defined(_WIN32) && defined(_WIN32_WINNT) && _WIN32_WINNT < 0x0602
+#if defined(_M_X64) && !defined(_AMD64_)
+#define _AMD64_ 1
+#elif defined(_M_IX86) && !defined(_X86_)
+#define _X86_ 1
+#elif defined(_M_ARM64) && !defined(_ARM64_)
+#define _ARM64_ 1
+#elif defined(_M_ARM) && !defined(_ARM_)
+#define _ARM_ 1
+#endif
+
 #include <winnt.h>
 
 typedef struct _SOCKET_PROCESSOR_AFFINITY {
